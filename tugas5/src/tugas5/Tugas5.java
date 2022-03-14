99787dd5bd7ea52037103a080d9b261b5280c18e
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2022-03-14 21:56:18 +0700

    Add files via upload

---
+++ tugas5/src/tugas5/Tugas5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas5;
+
+/**
+ *
+ * @author mallia
+ */
+public class Tugas5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFRM n = new JFRM ();
+        n.setVisible(true);
+    }
+    
+}
